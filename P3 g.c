65995fce612132fdd56e420945023a3001ea58f5
harshithgowda83282 <harshithgowda83282@gmail.com>
2025-11-19 10:22:44 +0530

    Implement comma operator example in C program
    This program demonstrates the use of the comma operator in C.

--- P3 g.c
+++ P3 g.c
@@ -1,3 +1,19 @@
-int a = 10, b = 3;
-printf("%d", a + b);   // 13
-printf("%d", a % b);   // 1
+//comma operation in c program
+#include <stdio.h>
+int main() {
+    int a, b, c, result;
+
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Using comma operator
+    result = (a += 5, b += 10, c += 15);
+
+    printf("After adding values using comma operator:\n");
+    printf("a: %d\n", a);
+    printf("b: %d\n", b);
+    printf("c: %d\n", c);
+    printf("Result (value of last expression): %d\n", result);
+
+    return 0;
+}
