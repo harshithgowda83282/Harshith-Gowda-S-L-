11e8eefd7667f06990861c9f1b3858ce505d0a71
harshithgowda83282 <harshithgowda83282@gmail.com>
2025-11-19 10:20:18 +0530

    Update P3 f.c

--- P3 f.c
+++ P3 f.c
@@ -1,2 +1,15 @@
-int a = 10, b = 20;
-int max = (a > b) ? a : b; // 'max' will be 20
+//conditional operation in c
+#include <stdio.h>
+int main() {
+    int a, b, max;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Using conditional (ternary) operator to find the maximum
+    max = (a > b) ? a : b;
+
+    printf("The maximum number is: %d\n", max);
+
+    return 0;
+}
