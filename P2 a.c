c2dd4e4e30fb978cfb99ba6366322555b9d54e4b
harshithgowda83282 <harshithgowda83282@gmail.com>
2025-11-19 07:53:02 +0530

    Add basic C program to display variable types

---
+++ P2 a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int integerValue = 10;
+    float floatValue = 3.14;
+    char charValue = 'A';
+    double doubleValue = 9.87654321;
+    printf("Integer: %d\n", integerValue);
+    printf("Float: %f\n", floatValue);
+    printf("Character: %c\n", charValue);
+    printf("Double: %lf\n", doubleValue);
+    return 0;
+}
